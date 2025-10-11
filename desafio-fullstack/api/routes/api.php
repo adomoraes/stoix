9a80e7ebd68aa870002bc1cc602743184429e6c4
Eduardo Moraes <adomoraes@gmail.com>
2025-10-11 19:52:53 -0300

    feat: Implement backend CRUD for tasks

--- desafio-fullstack/api/routes/api.php
+++ desafio-fullstack/api/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ContratoController;
 use App\Http\Controllers\PlanController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -30,3 +31,5 @@ Route::apiSingleton('user', UserController::class, ['only' => 'show']);
 
 Route::get('contratos/calculate/{plan}', [ContratoController::class, 'calculate']);
 Route::apiResource('contratos', ContratoController::class);
+
+Route::apiResource('tasks', TaskController::class);
