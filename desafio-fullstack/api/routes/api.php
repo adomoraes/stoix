6484be5d402ff4b2456cc61a05d8ac4e83abf194
Eduardo Moraes <adomoraes@gmail.com>
2025-10-11 19:57:23 -0300

    refactor: Remover rotas não utilizadas
    - Rotas do PlanController removidas
    - Rotas do UserController removidas
    - Rotas do ContratoController removidas

--- desafio-fullstack/api/routes/api.php
+++ desafio-fullstack/api/routes/api.php
@@ -1,9 +1,5 @@
 <?php
-
-use App\Http\Controllers\ContratoController;
-use App\Http\Controllers\PlanController;
 use App\Http\Controllers\TaskController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,12 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json(['message' => 'ok']);
 });
-
-Route::apiResource('plans', PlanController::class, ['only' => 'index']);
-
-Route::apiSingleton('user', UserController::class, ['only' => 'show']);
-
-Route::get('contratos/calculate/{plan}', [ContratoController::class, 'calculate']);
-Route::apiResource('contratos', ContratoController::class);
 
 Route::apiResource('tasks', TaskController::class);
