5aab46412612c64354086af7c3eebd8745400915
Adomoraes <adomoraes@gmail.com>
2025-10-13 14:40:45 -0300

    feat(auth): Implementar API de login e logout
    - Adicionado AuthController
    - Adicionado modelo de usuário
    - Configuradas rotas de API

--- desafio-fullstack/api/routes/api.php
+++ desafio-fullstack/api/routes/api.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/', function () {
-    return response()->json(['message' => 'ok']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::apiResource('tasks', TaskController::class);
 });
-
-Route::apiResource('tasks', TaskController::class);
