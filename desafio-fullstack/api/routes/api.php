ee42db287870f75018e4822878afc67db291c9a8
Adomoraes <adomoraes@gmail.com>
2025-10-13 22:55:37 -0300

    fix(auth): Correção de logout e middleware de autenticação
    - Correção de logout para usar o web guard
    - Adicionada resposta de API não autenticada
    - Atualização do uso do middleware de autenticação

--- desafio-fullstack/api/routes/api.php
+++ desafio-fullstack/api/routes/api.php
@@ -1,6 +1,8 @@
 <?php
+
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', function (Request $request) {
         return $request->user();
