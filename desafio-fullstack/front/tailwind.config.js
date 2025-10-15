38078dfa626f25b9cddc686394fa7ef2863a2499
Adomoraes <adomoraes@gmail.com>
2025-10-15 12:43:05 -0300

    feat: Adicionar animação de flash mágico
    - Adicionados quadros-chave para flash mágico
    - Configurada propriedade de animação

--- desafio-fullstack/front/tailwind.config.js
+++ desafio-fullstack/front/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'magic-flash': {
+          '0%, 100%': { 'box-shadow': '0 0 0 0 rgba(192, 132, 252, 0)' },
+          '50%': { 'box-shadow': '0 0 15px 5px rgba(192, 132, 252, 0.7)' },
+        }
+      },
+      animation: {
+        'magic-flash': 'magic-flash 1s ease-in-out',
+      }
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 }
