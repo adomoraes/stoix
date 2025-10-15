37a95c6cf85dc362039b8e7f8f6684176daf5d93
Adomoraes <adomoraes@gmail.com>
2025-10-15 13:38:28 -0300

    feat: Atualizar página de login com novo design
    - Título atualizado para "Aplicativo de Lista de Tarefas"
    - Rótulos alterados para português
    - Cor do espaço reservado atualizada

--- desafio-fullstack/front/src/App.tsx
+++ desafio-fullstack/front/src/App.tsx
@@ -4,9 +4,10 @@ import { Router } from "./Router"
 
 export function App() {
 	return (
-		<BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+		<BrowserRouter
+			future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
 			<AuthProvider>
-				<main className='pt-16'>
+				<main>
 					<Router />
 				</main>
 			</AuthProvider>
